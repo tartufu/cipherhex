271202c4da88c8fa1fed3ce7ba3e5ee56096b669
tartufu <ainurlindale@gmail.com>
2020-08-01 01:07:09 +0800

    split state into 4 rows for refactoring

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -17,8 +17,13 @@ function CardGrid() {
 
     useEffect(() => {
 
+        
         let cardArray = [];
         let twentyCards = [];
+        let rowOne = [];
+        let rowTwo = [];
+        let rowThree = [];
+        let rowFour = [];
 
         async function cardDeckCreation() {
 
@@ -55,59 +60,47 @@ function CardGrid() {
                 twentyCards.push(cardArray[i]);
             }
 
+            //Splitting cards into 4 rows 5 columns
+            for (let i = 0; i < 5; i++) {
+                rowOne.push(twentyCards[i]);
+            }
+
+            for (let i = 5; i < 10; i++) {
+                rowTwo.push(twentyCards[i]);
+            }
+
+            for (let i = 10; i < 15; i++) {
+                rowThree.push(twentyCards[i]);
+            }
+
+            for (let i = 15; i < 20; i++) {
+                rowFour.push(twentyCards[i]);
+            }
+
             console.log("asdasdas", cardArray);
             console.log("game startoooo", twentyCards);
+            console.log("game row oneeeeee", rowOne);
+            console.log("game row twwwoooooo", rowTwo);
+            console.log("game row threeeeeee", rowThree);
+            console.log("game row foooooourrrrrrr", rowFour );
+
             // return response;
+
+            addCards([rowOne, rowTwo, rowThree, rowThree]);
+
+            console.log(cardDeck);
+
         }
-
         cardDeckCreation();
-
-        //P1 Results
-        // fetch('https://pixabay.com/api/?key=2469399-5399347fa64ad7f4980f9b701&image_type=illustration&orientation=horizontal&q=fantasy&page=1&per_page=200&order=latest')
-        //     .then(response => response.json())
-        //     .then( (data) => {
-        //         let arr = data.hits
-        //         for (let i = 0; i < data.hits.length; i++) {
-        //             cardArray.push(data.hits[i]);
-        //         }
-        //         let shuffledArray = shuffleArray(cardArray);
-        //         console.log(cardArray);
-        //         addCards(shuffledArray);
-        //     });
-
-        // //P2 Results
-        // fetch('https://pixabay.com/api/?key=2469399-5399347fa64ad7f4980f9b701&image_type=illustration&orientation=horizontal&q=fantasy&page=2&per_page=200&order=latest')
-        // .then(response => response.json())
-        // .then( (data) => {
-        //     let arr = data.hits
-        //     for (let i = 0; i < data.hits.length; i++) {
-        //         cardArray.push(data.hits[i]);
-        //     }
-        //     let shuffledArray = shuffleArray(cardArray);
-        //     console.log(cardArray);
-        //     addCards(shuffledArray);
-        // });
-
-        // //All Time popular results
-        // fetch('https://pixabay.com/api/?key=2469399-5399347fa64ad7f4980f9b701&image_type=illustration&orientation=horizontal&q=fantasy&page=1&per_page=200&order=popular')
-        // .then(response => response.json())
-        // .then( (data) => {
-        //     let arr = data.hits
-        //     for (let i = 0; i < data.hits.length; i++) {
-        //         cardArray.push(data.hits[i]);
-        //     }
-        //     let shuffledArray = shuffleArray(cardArray);
-        //     console.log(cardArray);
-        //     addCards(shuffledArray);
-        // });
-
-        // console.log(cardArray);
     }, []);
 
     if (cardDeck.length !== 0) {
         return(
             <div className="container">
-                <div className="columns">
+                <p>asdsadasd</p>
+                <img src={cardDeck[0][0].webformatURL}></img> 
+
+                {/* <div className="columns">
                     <div className="column">
                         <button className="button is-large is-primary">asdasd </button>
                         <img src={cardDeck[0].webformatURL}></img> 
@@ -144,7 +137,7 @@ function CardGrid() {
                         <button className="button is-large is-primary">asdasd </button>
                         <img src={cardDeck[3].webformatURL}></img> 
                     </div>
-                </div>
+                </div> */}
             </div>
         )
     } else {
@@ -158,16 +151,4 @@ export default CardGrid;
 
 // https://daveceddia.com/usestate-hook-examples/
 // https://stackoverflow.com/questions/53715465/can-i-set-state-inside-a-useeffect-hook
-
-{/* <p>asdasdsdasd</p>
-<h1> {cardDeck[0].id}</h1>
-<img src={cardDeck[0].webformatURL}></img> */}
-
-
-// let response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-// let data = await response.json();
-// console.log(data);
-
-// let response1 = await fetch('https://jsonplaceholder.typicode.com/posts/2');
-// let data2 = await response1.json();
-// console.log(data2);
+// https://dev.to/shoupn/javascript-fetch-api-and-using-asyncawait-47mp
